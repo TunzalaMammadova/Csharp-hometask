575348ea7c4543b3948d5c158d5268ff3267c687
TunzalaMammadova <tunzalatma@code.edu.az>
2023-09-16 00:55:52 +0400

    added homeproject file

---
+++ CSHARP.TASK/Program.cs
@@ -0,0 +1,222 @@
+﻿//Task 1
+
+//int n = 23;
+
+//if (n % 2 == 0) 
+//{
+//    Console.WriteLine("even");
+//}
+//else
+//{
+//    Console.WriteLine("odd");
+//}
+
+
+
+//Task 2
+
+//int m = 26;
+
+//for (int i = 1; i <= m; i++)
+
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+
+////Task 3
+
+//int t = 12;
+//int sum = 0;
+
+//for (int i = 1; i <= t; i++)
+//{
+//    sum += i;
+
+//}
+//Console.WriteLine(sum);
+
+
+//Task 4
+
+//int m = 6;
+//int count = 0;
+
+
+//for(int i = 1; i<=m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }      
+//}
+//Console.WriteLine(count);
+
+
+//Task 5
+
+//int n = 2;
+//int m = 9;
+//int sum = 0;
+
+//for (int i = n ; i<=m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum += i;
+//    }
+
+//}
+//Console.WriteLine(sum);
+
+//Task 6
+
+//int a = 6;
+//int multiply = 1;
+
+//for (int i = 1; i<= a; i++)
+//{
+//    multiply *= i;
+
+//}
+
+//Console.WriteLine(multiply);
+
+
+//Task  7
+
+
+//int[] nums = { 2, 5, 4, 6, 2 };
+
+//int multiply = 1;
+
+
+//for(int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0)
+//  {
+//       multiply *= nums[i];
+
+//  }
+//}
+
+//Console.WriteLine(multiply);
+
+
+
+//Task 8
+
+
+//int s = 19;
+
+//if (s % 3 == 0 && s % 7 == 0)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+
+//Task 9
+
+//int a = 14;
+//int b = 6;
+//int sum = 0;
+
+
+//if (a % 2 == 0 && b % 2 == 0)
+//{
+//    sum = a + b;
+
+//}
+//Console.WriteLine(sum);
+
+
+
+//Task 10
+
+//int n = 2;
+//int m = 9;
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+
+//}
+
+//Console.WriteLine(sum);
+
+
+
+//Task 11
+
+
+//int[] nums = { 1, 3, 9, 5, 4, 6 };
+//int sum = 0;
+
+
+//for ( int i = 0; i< nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 1)
+//    {
+//        sum += nums[i];
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+
+//Task 12
+
+
+//int[] nums = { 2, 4, 3, 8,7,14, 5 };
+//int count = 0;
+
+//for (int i = 0; i<nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 0)
+//    {
+//        count++;
+//    }
+
+//}
+
+//Console.WriteLine(count);
+
+
+
+
+//Task 13
+
+
+//int n = 17;
+//int count = 0;
+
+
+//for (int i = 1; i <= n; i++)
+//{
+//    if (n % i == 0)
+//    {
+//        count++;
+//    }
+//}
+//    if (count == 2)
+//{
+//    Console.WriteLine("simple");
+
+//}
+
+//    else
+//{
+//    Console.WriteLine("complex");
+
+//}
